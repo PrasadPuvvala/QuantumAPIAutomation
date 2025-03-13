5f671e5fc5faefe0085976a4777678dd2a43ec05
AnjiMotapothula <vmotapothula@gnhearing.com>
2025-03-13 11:57:48 +0530

    test

--- obj/Release/net9.0/APIAutomation.AssemblyInfo.cs
+++ obj/Release/net9.0/APIAutomation.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIAutomation")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6d4332ff69e257af0204bcf19c900da15e48fd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIAutomation")]
